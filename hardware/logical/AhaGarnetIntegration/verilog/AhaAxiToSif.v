3729309718d163463d852a517791be5e585bed3d
Gedeon Nyengele <nyengele@stanford.edu>
2020-05-08 11:42:17 -0700

    registered outputs on SoC - GLB interface

--- hardware/logical/AhaGarnetIntegration/verilog/AhaAxiToSif.v
+++ hardware/logical/AhaGarnetIntegration/verilog/AhaAxiToSif.v
@@ -66,12 +66,27 @@ module AhaAxiToSif #(
   input   wire            SIF_RD_VALID
 );
 
-  wire unused = (| AWLOCK )  |
-                (| AWCACHE ) |
-                (| AWPROT )  |
-                (| ARLOCK )  |
-                (| ARCACHE ) |
-                (| ARPROT );
+wire unused = (| AWLOCK )  |
+              (| AWCACHE ) |
+              (| AWPROT )  |
+              (| ARLOCK )  |
+              (| ARCACHE ) |
+              (| ARPROT );
+
+  // Internal
+  wire [31:0]     sif_wr_addr_w;
+  wire            sif_wr_en_w;
+  wire [7:0]      sif_wr_strb_w;
+  wire [63:0]     sif_wr_data_w;
+  wire [31:0]     sif_rd_addr_w;
+  wire            sif_rd_en_w;
+
+  reg  [31:0]     sif_wr_addr_r;
+  reg             sif_wr_en_r;
+  reg  [7:0]      sif_wr_strb_r;
+  reg  [63:0]     sif_wr_data_r;
+  reg  [31:0]     sif_rd_addr_r;
+  reg             sif_rd_en_r;
 
 
   AhaAxiToSifWrite #(.ID_WIDTH(ID_WIDTH)) u_axi_to_sif_write (
@@ -97,10 +112,10 @@ module AhaAxiToSif #(
     .BVALID           (BVALID),
     .BREADY           (BREADY),
 
-    .SIF_ADDR         (SIF_WR_ADDR),
-    .SIF_WE           (SIF_WR_EN),
-    .SIF_STRB         (SIF_WR_STRB),
-    .SIF_DATA         (SIF_WR_DATA)
+    .SIF_ADDR         (sif_wr_addr_w),
+    .SIF_WE           (sif_wr_en_w),
+    .SIF_STRB         (sif_wr_strb_w),
+    .SIF_DATA         (sif_wr_data_w)
   );
 
   AhaAxiToSifRead #(.ID_WIDTH(ID_WIDTH)) u_axi_to_sif_read (
@@ -122,10 +137,38 @@ module AhaAxiToSif #(
     .RREADY         (RREADY),
     .RDATA          (RDATA),
 
-    .SIF_RD_ADDR    (SIF_RD_ADDR),
-    .SIF_RD_EN      (SIF_RD_EN),
+    .SIF_RD_ADDR    (sif_rd_addr_w),
+    .SIF_RD_EN      (sif_rd_en_w),
     .SIF_RD_DATA    (SIF_RD_DATA),
     .SIF_RD_VALID   (SIF_RD_VALID)
   );
+
+  // Register Outputs
+  always @(posedge ACLK or negedge ARESETn) begin
+    if(~ARESETn) begin
+      sif_wr_addr_r     <= 32'h0;
+      sif_wr_en_r       <= 1'b0;
+      sif_wr_strb_r     <= 8'h0;
+      sif_wr_data_r     <= 64'h0;
+      sif_rd_addr_r     <= 32'h0;
+      sif_rd_en_r       <= 1'b0;
+    end else begin
+      sif_wr_addr_r     <= sif_wr_addr_w;
+      sif_wr_en_r       <= sif_wr_en_w;
+      sif_wr_strb_r     <= sif_wr_strb_w;
+      sif_wr_data_r     <= sif_wr_data_w;
+      sif_rd_addr_r     <= sif_rd_addr_w;
+      sif_rd_en_r       <= sif_rd_en_w;
+    end
+  end
+
+  // Assign Outputs
+  assign SIF_WR_ADDR  = sif_wr_addr_r;
+  assign SIF_WR_EN    = sif_wr_en_r;
+  assign SIF_WR_STRB  = sif_wr_strb_r;
+  assign SIF_WR_DATA  = sif_wr_data_r;
+
+  assign SIF_RD_ADDR  = sif_rd_addr_r;
+  assign SIF_RD_EN    = sif_rd_en_r;
 
 endmodule
