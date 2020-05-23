f5e7fa53dee702ff8ac09ab077d9df08ce31936f
Gedeon Nyengele <nyengele@stanford.edu>
2020-05-22 19:14:04 -0700

    updated master clock switch to switch between async master clocks

--- hardware/logical/AhaPlatformController/verilog/AhaClockSwitchSlice.v
+++ hardware/logical/AhaPlatformController/verilog/AhaClockSwitchSlice.v
@@ -21,15 +21,20 @@ module AhaClockSwitchSlice (
 );
 
   // Internal
-  reg     clk_sel;
-  reg     clk_en;
+  reg     clk_sel_q;
+  reg     clk_sel_qq;
+  reg     clk_en_q;
+  reg     clk_en_qq;
 
   always @ (negedge CLK) begin
-    clk_sel   <= SELECT_REQ;
-    clk_en    <= SELECT_REQ & ~OTHERS_SELECT;
+    clk_sel_q     <= SELECT_REQ;
+    clk_en_q      <= SELECT_REQ & ~OTHERS_SELECT;
+
+    clk_sel_qq    <= clk_sel_q;
+    clk_en_qq     <= clk_en_q;
   end
 
-  assign CLK_OUT  = CLK & clk_en;
-  assign SELECT_ACK = clk_sel;
+  assign CLK_OUT  = CLK & clk_en_qq;
+  assign SELECT_ACK = clk_sel_qq;
 
 endmodule
