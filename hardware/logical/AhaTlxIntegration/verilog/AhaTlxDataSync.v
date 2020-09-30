e78a515aabe8775c6ccea375ef7950a4e352e3b6
Alex Carsello <alex.carsello@gmail.com>
2020-09-29 17:08:01 -0700

    First pass at instantiating CW_data_sync_1c

--- hardware/logical/AhaTlxIntegration/verilog/AhaTlxDataSync.v
+++ hardware/logical/AhaTlxIntegration/verilog/AhaTlxDataSync.v
@@ -21,7 +21,8 @@ module AhaTlxDataSync #(
   // Outputs
   output  wire  [WIDTH-1:0] Q
 );
-
+  // For Synopsys DW IP
+  /*
   DW_data_sync #(
     .width          (WIDTH),
     .pend_mode      (1),
@@ -45,6 +46,34 @@ module AhaTlxDataSync #(
     .data_avail_d   (),
     .data_d         (Q),
     .test           (1'b0)
+  );
+  */
+  // For Cadence CW IP 
+  CW_data_sync_1c #(
+    .width          (WIDTH),
+    .f_sync_type    (2),
+    .tst_mode       (0),
+    .verif_en       (0),
+    .filt_size      (1)
+    )
+  u_aha_tlx_data_sync (
+    .clk_d          (DEST_CLK),
+    .rst_d_n        (DEST_RESETn),
+    .init_d_n       (1'b1),
+    .data_s         (D),
+    .filt_d         (1'b1),
+    .test           (1'b0),
+    .data_avail_d   (),
+    .data_d         (Q),
+    .max_skew_d     ()
+    // CW Synchronizer doesn't use source clock
+    //.clk_s          (SRC_CLK),
+    //.rst_s_n        (SRC_RESETn),
+    //.init_s_n       (1'b1),
+    //.send_s         (1'b1),
+    //.empty_s        (),
+    //.full_s         (),
+    //.done_s         (),
   );
 
 endmodule
