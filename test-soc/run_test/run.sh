ac2c367fd239f73dd41e8ddee0549e7f2273e63f
Taeyoung Kong <kongty@stanford.edu>
2020-10-08 14:40:42 -0700

    Add exit to command

--- test-soc/run_test/run.sh
+++ test-soc/run_test/run.sh
@@ -9,6 +9,7 @@ ln -s ${CXDT} CXDT.bin
 ln -s ${XCELIUMD} xcelium.d
 
 touch image.hex
-echo "run -timepoint ${MAX_SIM_TIME}" > commands.tcl
+echo "run" > commands.tcl
+echo "exit" >> commands.tcl
 
 xrun -R -input commands.tcl | tee outputs/xrun_run_${TEST_NAME}.log
